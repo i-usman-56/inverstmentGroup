5348b53855db97d022da199c67eba44a6e6d8b47
Muhammad Usman <128576571+i-usman-56@users.noreply.github.com>
2024-06-06 11:26:21 +0500

    Update LoginHeader.jsx

--- src/components/signup/components/LoginHeader.jsx
+++ src/components/signup/components/LoginHeader.jsx
@@ -5,10 +5,10 @@ export default function LoginHeader({ img, title, desc }) {
     <div>
       <div className="flex flex-col justify-center items-center">
         <img src={img} alt="" />
-        <h1 className="text-[20px] lg:text-[25px] xl:text-[35px] pt-[20px] leading-[22px] lg:leading-7 tracking-[-1.7%] font-bold text-[#3C3C3C]">
+        <h1 className="text-[20px] lg:text-[25px] xl:text-[35px] pt-[16px] lg:pt-[18px] leading-[22px] lg:leading-7 tracking-[-1.7%] font-bold text-[#3C3C3C]">
           {title}
         </h1>
-        <p className="text-[14px] lg:text-[16px] pt-[22px] leading-[16.7px] tracking-[-1.7%] font-bold text-[#3C3C3C]">
+        <p className="text-[14px] lg:text-[16px] pt-[18px] lg:pt-[22px]  leading-[16.7px] tracking-[-1.7%] font-bold text-[#3C3C3C]">
           {desc}
         </p>
       </div>
