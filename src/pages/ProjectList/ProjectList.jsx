3e4e205776d7f280c1a3d047bb38368bd78fd3b3
Tanzeel Ahmed <tanzeel152@gmail.com>
2024-06-26 11:17:01 +0500

    feat: prospect screen responsive

--- src/pages/ProjectList/ProjectList.jsx
+++ src/pages/ProjectList/ProjectList.jsx
@@ -4,10 +4,10 @@ import "../../components/task/Task.css";
 
 export default function ProjectList() {
   return (
-    <div className="flex items-start">
+    <div className="lg:flex lg:items-start">
 
       {/*MOBILE FILTER */}
-      <div className="bg-blue-500 p-4 text-white w-full md:hidden">
+      <div className="bg-blue-500 p-4 text-white w-full lg:hidden text-center">
         <select className="bg-white text-gray-800 p-2 rounded">
           <option>PROSPECTS BY FILTER</option>
           <option>My Prospects</option>
@@ -21,12 +21,14 @@ export default function ProjectList() {
         </select>
       </div>
 
-      <div className="w-64 bg-white p-4 ml-4 hidden md:block mt-4 rounded-lg">
+      <div className="w-64 bg-white p-4 ml-4 hidden lg:block mt-4 rounded-lg shadow-lg">
         <div className="font-bold text-lg mb-4 text-[#0250E6]">PROSPECTS BY FILTER</div>
         <div className="space-y-6">
           {[
             { color: "blue-500", text: "My Prospects ▼" },
-            { color: "gray-700", text: "Closing Status ▼" },
+            {
+              color: "gray-900", text: "Closing Status ▼"
+            },
             { color: "green-500", text: "Interest ▼" },
             { color: "blue-300", text: "Open Prospects" },
             { color: "purple-500", text: "By Scheduled Event ▼" },
@@ -34,7 +36,7 @@ export default function ProjectList() {
             { color: "yellow-500", text: "Shared With Me" },
             { color: "red-500", text: "Dead Leads" },
           ].map((item, index) => (
-            <div key={index}>
+            <div key={index} className="flex items-center">
               <span className={`inline-block w-4 h-4 bg-${item.color} mr-2`}></span>
               <span className="text-black">{item.text}</span>
             </div>
@@ -43,7 +45,7 @@ export default function ProjectList() {
         <div className="font-bold text-lg mt-8 text-[#0250E6]">CURRENT CLIENTS</div>
         <div className="space-y-4">
           <div>
-            <span className="inline-block w-4 h-4 bg-blue-500 mr-2"></span>
+            <span className="inline-block w-4 h-4 bg-blue-500 mr-2 mt-4"></span>
             <span className="text-black">Closed & Ongoing Clients</span>
           </div>
           <div>
--- src/pages/ProjectList/component/Table.jsx
+++ src/pages/ProjectList/component/Table.jsx
@@ -7,7 +7,7 @@ export default function Table({ tableData }) {
     return rows.map((rowData, index) => (
       <div
         key={index}
-        className="bg-white mt-[5px] flex flex-col lg:flex-row items-start lg:items-center border border-[#D9D9D9] rounded-md"
+        className="bg-white mt-[5px] flex lg:flex-row items-start lg:items-center border border-[#D9D9D9] rounded-md"
       >
         {rowData.map((data, dataIndex) => (
 
@@ -44,27 +44,30 @@ export default function Table({ tableData }) {
 
   return (
     <>
-      <div className="lg:px-[0px] md:px-8  bg-white p-5 rounded-lg">
-        <div className="flex justify-between w-full items-center pl-[37px] pr-[24px] md:pl-0 md:pr-0 lg:pl-0 lg:pr-0 mt-[20px]">
-          <h1 className="text-[17px] lg:text-[28px] font-[700] leading-[17.64px] tracking-[-1.7%] text-[#0250E6] ml-5">
-            My Prospects
-          </h1>
+      <div className="relative">
+        <img src="../../src/assets/img/startCalling.png" alt="Top Right Image" className="absolute -top-3 -right-3 w-[50px] h-[50px]" />
+        <div className="lg:px-[0px] md:px-8  bg-white p-5 rounded-lg">
+          <div className="flex justify-between w-full items-center pl-[37px] pr-[24px] md:pl-0 md:pr-0 lg:pl-0 lg:pr-0 mt-[20px]">
+            <h1 className="text-[17px] lg:text-[28px] font-[700] leading-[17.64px] tracking-[-1.7%] text-[#0250E6] ml-5">
+              My Prospects
+            </h1>
 
-        </div>
-        <div className="overflowContainer mx-[22px] lg:mx-0 mt-[24px] lg:mt-[30px]">
-          <div className="w-[725px] lg:w-full lg:pl-16 xl:pl-20">
-            <div className="flex w-full mb-[12px] lg:mb-[16px]">
-              {columns.map((columnName, index) => (
-                <p
-                  key={index}
-                  className="text-[17px] w-[20%] text-center font-[700] leading-[17.64px] tracking-[-1.7%] text-[#0250E6] p-2"
-                >
-                  {columnName}
-                </p>
-              ))}
-            </div>
-            <div className="lg:h-full h-[100px] overflowContainerY">
-              <RenderData />
+          </div>
+          <div className="overflowContainer  lg:mx-0 mt-[24px] lg:mt-[30px]">
+            <div className="w-[725px] lg:w-full lg:pl-16 xl:pl-20">
+              <div className="flex w-full mb-[12px] lg:mb-[16px]">
+                {columns.map((columnName, index) => (
+                  <p
+                    key={index}
+                    className="text-[17px] w-[20%] text-center font-[700] leading-[17.64px] tracking-[-1.7%] text-[#0250E6] p-2"
+                  >
+                    {columnName}
+                  </p>
+                ))}
+              </div>
+              <div className="lg:h-full h-full overflowContainerY">
+                <RenderData />
+              </div>
             </div>
           </div>
         </div>
