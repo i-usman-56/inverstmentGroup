374793a9654e02e4ee44d0093269ccbae29da8b8
Muhammad Usman <128576571+i-usman-56@users.noreply.github.com>
2024-08-02 10:38:53 +0500

    Update forgetPAssword.jsx

--- src/components/auth/forgetPassword/forgetPAssword.jsx
+++ src/components/auth/forgetPassword/forgetPAssword.jsx
@@ -73,11 +73,6 @@ export default function ForgetPAssword() {
                   error={errors.email}
                   autocomplete="email" // Updated here
                 />
-                {errors && (
-                  <p className="text-[16px] font-semibold text-[#a10d0d]">
-                    {errors.email}
-                  </p>
-                )}
               </div>
             </div>
             <div className="mt-[40px]">
