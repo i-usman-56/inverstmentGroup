a4e6d1c01fde4bb1306e723a98e802812f46ab0c
Muhammad Usman <128576571+i-usman-56@users.noreply.github.com>
2024-08-05 17:48:12 +0500

    changes

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = 'http://localhost:5001';
+const URL = 'http://65.109.118.136:4747';
 
 export const socket = io(URL);
